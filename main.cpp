853c7228b853a5bf8b193a70b73ece146ba0e312
david <david@davidasus>
2014-11-25 23:22:34 +0100

    Premier commit (ajout du main et Makefile : vide)

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: david
+ *
+ * Created on 25 novembre 2014, 23:20
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+    return 0;
+}
+
