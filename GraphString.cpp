c50fcfbd3aa1d336721ff617c2c775aaa928853b
Touzard Loic <loic@oblady.fr>
2014-12-06 18:54:58 +0100

    Avancement sur GraphString + ajout  des iterator (outer et inner) pour le graph

--- GraphString.cpp
+++ GraphString.cpp
@@ -21,6 +21,9 @@ using namespace std;
 //---------------------------------------------------- Variables de classe
 
 //----------------------------------------------------------- Types privés
+typedef map<string, int> Inner_map; //map interne, contenant la cible et son nombre de liens
+typedef pair<Inner_map, int> Inner_pair;
+typedef map<string, Inner_pair> Outer_map;
 
 
 //----------------------------------------------------------------- PUBLIC
@@ -33,13 +36,74 @@ using namespace std;
 //{
 //} //----- Fin de Méthode
 
+void GraphString::Insert(string referer, string target)
+{
+    if(graph.find(target) != graph.end())
+    {
+        cout << "OUI" << endl;
+        // la clé existe
+        Inner_pair* currentPair = &graph[target];
+        Inner_map* currentInnerMap = &currentPair->first;
+        currentPair->second++;// on incrémente le compteur de liens totaux
+        if(currentInnerMap->find(referer) != currentInnerMap->end())
+        {
+            // le liens existe
+            (*currentInnerMap)[referer]++;
+        }
+        else
+        {
+            // le liens n'existe pas, on l'insere
+            (*currentInnerMap)[referer]++;
+        }
+
+    }
+    else
+    {
+        //la clé n'existe pas, on la créée
+        cout << "NON" << endl;
+        Inner_map newMap;
+        newMap[referer] = 1;
+        Inner_pair newPair(newMap, 1);
+        graph[target] = newPair;
+    }
+}
+
+
+int GraphString::GetLinks(string referer, string target)
+{
+    return graph[target].first[referer];
+}
+
+int GraphString::GetAllLinks(string target)
+{
+    return graph[target].second;
+}
+
+int GraphString::Size()
+{
+    return graph.size();
+}
+
+void GraphString::Display()
+{
+    for(Outer_cIterator i = graph.begin(), iend = graph.end(); i != iend; ++i)//du debut a la fin du graph
+    {
+        cout << "cible : (" << i->second.second << ")" << endl << i->first << endl;
+        cout << "\treferer : " << endl;
+        for (Inner_cIterator j = i->second.first.begin(), jend = i->second.first.end(); j != jend; ++j)
+        {
+               cout << "\t- " << j->first << "(" << j->second << ")" << endl;
+        }
+    }
+}
+
 
 //------------------------------------------------- Surcharge d'opérateurs
-GraphString & GraphString::operator = ( const GraphString & unGraphString )
-// Algorithme :
-//
-{
-} //----- Fin de operator =
+//GraphString & GraphString::operator = ( const GraphString & unGraphString )
+//// Algorithme :
+////
+//{
+//} //----- Fin de operator =
 
 
 //-------------------------------------------- Constructeurs - destructeur
