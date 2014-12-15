8199ef6fdab72de0804278f3e0e6a1c460c78700
dwobrock <david.wobrock@insa-lyon.fr>
2014-12-15 13:12:13 +0100

    Blabla rapport + cerr + rajout d'un test fonctionnel

--- main.cpp
+++ main.cpp
@@ -70,7 +70,7 @@ int main(int argc, char** argv)
 
     if(!apacheParser.IsGood())
     {
-        cout << apacheParser.GetLastError() << endl;
+        cerr << apacheParser.GetLastError() << endl;
         return -1;
     }
 
